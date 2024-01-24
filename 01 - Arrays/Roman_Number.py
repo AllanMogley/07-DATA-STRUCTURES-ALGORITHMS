e8e6e137c343df2fe3ab05dad67e2f0e91a0ba9f
AllanMogley <allanmogley@gmail.com>
2024-01-24 16:36:59 +0300

    Roman Numbers Success 1

--- 01 - Arrays/Roman_Number.py
+++ 01 - Arrays/Roman_Number.py
@@ -1,4 +1,5 @@
-def romanToInt(s: str) -> int:
+# def romanToInt(s: str) -> int:
+def romanToInt(s:str):
             mydict = {
                         'I' : 1, 'II' : 2, 'III' : 3,
                         'V' : 5, 'X' : 10,
@@ -6,11 +7,14 @@ def romanToInt(s: str) -> int:
                         'D' : 500, 'M' : 1000,
 
                         'IV' : 4, 'IX' : 9,
-                        'XL' : 40, 'XC' : 90,
+                        
                         'CD' : 400, 'CM' : 900}
-                                                                                                                    
-            mylist = [mydict.get(x) for x in range(len(s.split())) if s.split()[x] == mydict]
-            print(sum(mylist))
-            return(sum(mylist))
-s = 'LV'
+            
+
+            # mylist = mydict.get('L')
+            mylist = [mydict.get(s) for x in range(len(s.split())) if s.split()[x] in mydict]
+            print(mylist)
+            # return(sum(mylist))
+
+s = 'IX'
 romanToInt(s)
