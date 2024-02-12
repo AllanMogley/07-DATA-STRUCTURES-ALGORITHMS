632cfe2668f0a42634edb9bb8a7d1b149a021999
AllanMogley <allanmogley@gmail.com>
2024-02-12 08:25:31 +0300

    New Solution

---
+++ 03 - String Matching Algols/02 Substrings/substrings2.py
@@ -0,0 +1,13 @@
+class Test:
+    def prefix(self, s: str) -> str:
+
+        ans = ''
+        for i in range(len(s)):
+            while s[i] not in ans:
+                ans += s[i]
+        print('None ', ans)
+
+
+strs = Test()
+text = 'pwwkew'
+strs.prefix(text)
