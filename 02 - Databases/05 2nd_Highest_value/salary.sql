1fa4471c54958232e58f5dfe211e09f257dbf0af
AllanMogley <allanmogley@gmail.com>
2024-03-03 08:48:24 +0300

    Refined Code

--- 02 - Databases/05 2nd_Highest_value/salary.sql
+++ 02 - Databases/05 2nd_Highest_value/salary.sql
@@ -1,5 +1,3 @@
-select(
-	select distinct salary from Employee
-	order by salary desc
-	limit 1 offset 1
-) as SecondHighestSalary
+# Write your MySQL query statement below
+select(select distinct salary from Employee order by salary desc limit 1 offset 1) 
+    as SecondHighestSalary
