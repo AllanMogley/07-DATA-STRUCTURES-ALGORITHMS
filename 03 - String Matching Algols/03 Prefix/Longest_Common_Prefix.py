3cc7dfca218f9dba2c035f77f8314f155bc283d7
AllanMogley <allanmogley@gmail.com>
2024-03-09 11:51:15 +0300

    ERROR

--- 03 - String Matching Algols/03 Prefix/Longest_Common_Prefix.py
+++ 03 - String Matching Algols/03 Prefix/Longest_Common_Prefix.py
@@ -4,20 +4,21 @@ class Solution:
     def longestCommonPrefix(strs: List[str]) -> str:
 
         le = min([len(strs[i]) for i in range(len(strs))])
-        print(le)
+        print('Min Len ---> ', le)
         
-        ans = ''
+        ans = []
         first = strs[0]
         last = strs[-1]
 
         for i in range (le):
-            if first[i] != last[i]:
-                print('None')
-            ans += first[i]
-            print(ans)
+            if first[i] == last[i]:
+                ans += first[i]
+            else:
+                ans -= first[i]
+        print(ans)
 
 
-myl = ['flow', 'flowe', 'flower']
+myl = ['fliw', 'flowe', 'flower']
 ls = Solution
 ls.longestCommonPrefix(myl)
 
