aee455fab4d922507aea0e85863e68d6044cb5a5
AllanMogley <allanmogley@gmail.com>
2024-03-17 13:28:29 +0300

    Star Schema

--- 02 - Databases/02_SQL_Queries/02_Relations.sql
+++ 02 - Databases/02_SQL_Queries/02_Relations.sql
@@ -1,5 +1,5 @@
+alter table ids add primary key (i);
 alter table adj_close add primary key (id);
-alter table adj_close add foreign key (id) references close (id);
 alter table close add primary key (id);
 alter table high add primary key (id);
 alter table low add primary key (id);
@@ -9,6 +9,13 @@ alter table volume add primary key (id);
 
 
 
+alter table ids add foreign key (id) references adj_close (id);
+alter table ids add foreign key (id) references close (id);
+alter table ids add foreign key (id) references high (id);
+alter table ids add foreign key (id) references low (id);
+alter table ids add foreign key (id) references open (id);
+alter table ids add foreign key (id) references orgs (id);
+alter table ids add foreign key (id) references volume (id);
 
 
 
